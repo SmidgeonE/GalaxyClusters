3daf55216c62e242c96e113fbc0c5563752141c6
Smidgeon <finlaynugent@gmail.com>
2023-10-19 20:49:57 +0100

    Set up querying system for multiple clusters
    to do: add logic for size of ClG

--- Main.py
+++ Main.py
@@ -1,9 +1,12 @@
-# %%
 
 from matplotlib import pyplot as plt
 from Equations import *
 from astroquery.simbad import Simbad
+import pandas as pd
 import astropy.coordinates as coord
+import time
+
+previousQueryTime = time.time()
 
 
 def findClusters(saveToFile=False, name='clusterQuery.csv'):
@@ -11,11 +14,15 @@ def findClusters(saveToFile=False, name='clusterQuery.csv'):
     # Querying to find galaxy clusters
 
     customSimbad = Simbad()
+    customSimbad.add_votable_fields('z_value', 'ra', 'dec')
+    customSimbad.remove_votable_fields('coordinates')
 
-    qry = ("region(circle, 29.20 -0.214, 0.15d) &"
+    qry = ("region(circle, 29.20 -0.214, 0.3d) &"
            " otypes in ('ClG', 'C?G')")
     clusters = customSimbad.query_criteria(qry).to_pandas()
-    clusters = clusters.loc[:, ['MAIN_ID', 'RA', 'DEC']]
+    clusters = clusters.drop(['SCRIPT_NUMBER_ID'], axis=1)
+    # clusters = clusters.loc[:, ['MAIN_ID', 'RA', 'DEC']]
+
 
     print(clusters)
 
@@ -25,40 +32,52 @@ def findClusters(saveToFile=False, name='clusterQuery.csv'):
     return clusters
 
 
-def getClusterData(ra=227.745, dec=5.761):
+def getGalaxiesFromCluster(ra, dec):
+    global previousQueryTime
+
     # set up custom query and define which columns we want
     customSimbad = Simbad()
-    customSimbad.add_votable_fields('ra(d)', 'dec(d)', 'distance_result', 'otype', 'rv_value', 'z_value')
+    customSimbad.add_votable_fields('ra(d)', 'dec(d)',
+                                    'distance_result', 'otype', 'rv_value', 'z_value')
     customSimbad.remove_votable_fields('coordinates')
 
-    # use 20 arcmin as an example
-    my_radius = '0d20m0s'
+    my_radius = '0d2m0s'
 
-    # run query
+    currentQueryTime = time.time()
+
+    if currentQueryTime - previousQueryTime < 0.5:
+        print("waiting")
+        time.sleep(0.5-currentQueryTime+previousQueryTime)
+
     result_table = customSimbad.query_region(coord.SkyCoord(ra, dec, unit='deg'), radius=my_radius)
+    previousQueryTime = currentQueryTime
+
     df = result_table.to_pandas()
 
     # Select only galaxies
     gal_df = df[df['OTYPE'] == 'Galaxy']
-    return gal_df[(gal_df['Z_VALUE'] < 0.09) * (gal_df['Z_VALUE'] > 0.065)]
+
+    return gal_df
+
+    # This worked well for the a2029 galaxy
+    # return gal_df[(gal_df['Z_VALUE'] < 0.09) * (gal_df['Z_VALUE'] > 0.065)]
 
 
-cluster = getClusterData()
-
-
-def CalculateOmegaM(makeGraphs=False):
+def CalculateOmegaM(galaxiesOfCluster, nameOfGalaxy, makeGraphs=False):
 
     # Now we generate some graphs of the cluster
 
     fig2, ax2 = plt.subplots(1, 1)
-    ax2.hist(cluster['Z_VALUE'], bins=100)
+    ax2.hist(galaxiesOfCluster['Z_VALUE'], bins=100)
     ax2.set_ylabel('num')
     ax2.set_xlabel('Z_VALUE')
-    if makeGraphs: plt.show()
+    ax2.set_title('Z Values for ' + nameOfGalaxy)
+    if makeGraphs:
+        plt.show()
 
     # Now we plot a gaussian on the histogram for recession vel
 
-    recessionVel = cluster['RV_VALUE']
+    recessionVel = galaxiesOfCluster['RV_VALUE']
     fig, ax = plt.subplots(1, 1)
     histData = ax.hist(recessionVel, bins=100)
     ax.set_ylabel('num')
@@ -67,20 +86,35 @@ def CalculateOmegaM(makeGraphs=False):
     velocityRange = np.linspace(np.min(recessionVel), np.max(recessionVel), len(recessionVel))
     gaussVals, sd = gauss(recessionVel, np.max(histData[0]), velocityRange)
     ax.plot(velocityRange, gaussVals)
-    if makeGraphs: plt.show()
+    if makeGraphs:
+        plt.show()
 
-    averageZ = cluster['Z_VALUE'].mean()
+    averageZ = galaxiesOfCluster['Z_VALUE'].mean()
     M_200val = M_200(sd * 1000, averageZ)
     f_ourGas = M_gas / M_200val
     f_b = f_star + f_ourGas
     Omega_m = Omega_b / f_b
 
-    print("M_200 value: " + str(M_200val))
-    print("omega m : " + str(Omega_m))
+    print("\nDATA FOR : " + nameOfGalaxy)
+    print("--- M_200 value: " + str(M_200val))
+    print("--- omega m : " + str(Omega_m))
 
     return Omega_m
 
 
-# %%
+# c = findClusters(saveToFile=True)
 
-c = findClusters(saveToFile=True)
+clustersSet = pd.read_csv('clusterQuery.csv')
+
+for i in clustersSet.index:
+    clusterData = getGalaxiesFromCluster(clustersSet['RA'][i], clustersSet['DEC'][i])
+
+    if len(clusterData.index) < 10:
+        print("\nInsufficient Number of galaxies in cluster : " + str(clustersSet['MAIN_ID'][i]))
+        continue
+
+    print(clusterData)
+    CalculateOmegaM(clusterData, nameOfGalaxy=clustersSet['MAIN_ID'][i], makeGraphs=True)
+
+
+print("\nFinished Queries")
